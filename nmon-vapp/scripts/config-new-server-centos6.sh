d58c009fa20471ec0da3ec886c6203f730e5bbbc
Youngmo Kwon <youngmo.kwon777@gmail.com>
2015-08-11 12:55:06 +0000

    modify cent-os release version to 6 for mongodb repository

--- nmon-vapp/scripts/config-new-server-centos6.sh
+++ nmon-vapp/scripts/config-new-server-centos6.sh
@@ -122,7 +122,7 @@ yum install -y nodejs npm
 cat > /etc/yum.repos.d/mongodb-org-3.0.repo << EOF
 [mongodb-org-3.0]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.0/x86_64/
+baseurl=https://repo.mongodb.org/yum/redhat/6/mongodb-org/3.0/x86_64/
 gpgcheck=0
 enabled=1
 EOF
