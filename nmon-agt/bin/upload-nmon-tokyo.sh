e81bfda0e16bfde433417469ac4bf42e03546863
Youngmo Kwon <youngmo.kwon@fjint.com>
2015-08-17 20:16:59 +0900

    add upload scripts

---
+++ nmon-agt/bin/upload-nmon-tokyo.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150813_2013.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150814_0014.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150814_0415.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150814_0816.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150814_1216.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150814_1617.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150814_2000.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150815_0000.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150815_0400.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150815_0800.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150815_1200.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150815_1600.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150815_2000.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150816_0000.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150816_0400.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150816_0800.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150816_1200.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150816_1600.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150816_2000.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150817_0000.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150817_0400.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150817_0800.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150817_1200.nmon
+./nmon-upldr.sh nmonlog upload /DATA/nmon-capture/nmon-tokyo.fjint.com/nmon-tokyo_150817_1600.nmon
