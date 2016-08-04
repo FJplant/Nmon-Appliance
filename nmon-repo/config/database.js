b9767d2b4f8e1040604378f03bcf61c952e39958
Youngmo Kwon <youngmo.kwon777@gmail.com>
2016-08-05 07:24:57 +0900

    modify configurations and add node packages

--- nmon-repo/config/database.js
+++ nmon-repo/config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'your-database-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	//'url' : 'your-database-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+        'url' : 'mongodb://localhost:27017/nmon-user' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
