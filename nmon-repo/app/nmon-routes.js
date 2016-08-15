7e54c94fcb2f448761d8618adb912c569b15de47
Youngmo Kwon <youngmo.kwon777@gmail.com>
2016-08-15 20:38:20 +0900

    add authentication

--- nmon-repo/app/nmon-routes.js
+++ nmon-repo/app/nmon-routes.js
@@ -20,7 +20,8 @@ module.exports = function(app, passport) {
 
 	// process the login form
 	app.post('/login', passport.authenticate('local-login', {
-		successRedirect : '/profile', // redirect to the secure profile section
+		//successRedirect : '/profile', // redirect to the secure profile section
+		successRedirect : '/nmon-db', // redirect to the secure nmon-db section
 		failureRedirect : '/login', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
@@ -70,5 +71,5 @@ function isLoggedIn(req, res, next) {
 		return next();
 
 	// if they aren't redirect them to the home page
-	res.redirect('/');
-}
+	res.redirect('/login');
+};
---
+++ nmon-repo/nmon-server.js
@@ -0,0 +1,104 @@
+/*
+ * nmon-server.js is
+ *    an startup file kf nmon-repo component written in Node.js
+ *   and written by amoriya ( Junkoo Hea, junkoo.hea@gmail.com )
+ *                  ymk     ( Youngmo Kwon, youngmo.kwon777@gmail.com ) 
+ *
+ *      since Aug 12, 2015
+ * (c) All rights reserved to Junkoo Hea, Youngmo Kwon
+ */
+
+// set up ======================================================================
+// initialize cluster
+var cluster = require('cluster'),       // to manage multi process
+    os = require('os'),                 // to get CPU informations
+    winston = require('winston');
+
+/*
+ * Initialize winston logger
+ */
+var log = new (winston.Logger)({
+    transports: [
+        new (winston.transports.File)({ filename: 'logs/nmon-db.log', level: 'debug' }),
+    ]
+});
+
+/*
+ * Calculate cluster parameter
+ */
+var cpus = os.cpus(); // Get CPU informations
+var worker_cnt = cpus.length * 2; // # of Worker process = 2 * CPU count
+//var worker_cnt = cpus.length;     // # of Worker process = CPU count, for development purpose
+
+/*
+ * Fork worker process and listen service
+ */
+if (cluster.isMaster) {
+    log.info('CPU lists:');
+    for (var i=0; i< cpus.length; i++)
+        log.info('CPU #%d: %s', i, cpus[i].model);
+
+    log.info('Spawning %d worker process', worker_cnt);
+
+    for (var i=0; i < worker_cnt; i+=1) {
+        cluster.fork();
+    }
+    log.info('Server running at http://localhost:6900');
+
+    // Listen for dying workers
+    cluster.on('exit', function(worker) {
+        // Replace the dead worker
+        log.info('Worker %d died... :( respawining... ', worker.id);
+        cluster.fork();
+    });
+} else {
+    // get all the tools we need
+    var express  = require('express');
+    var app      = express();
+    var port     = process.env.PORT || 6900;
+    var mongoose = require('mongoose');
+    var passport = require('passport');
+    var flash    = require('connect-flash');
+
+    var morgan       = require('morgan');
+    var cookieParser = require('cookie-parser');
+    var bodyParser   = require('body-parser');
+    var session      = require('express-session');
+
+    var configDB = require('./config/database.js');
+
+    // configuration ===============================================================
+    mongoose.connect(configDB.url); // connect to our database
+
+    require('./config/passport')(passport); // pass passport for configuration
+
+    // set up our express application
+    app.use(morgan('dev')); // log every request to the console
+    app.use(cookieParser()); // read cookies (needed for auth)
+    app.use(bodyParser()); // get information from html forms
+
+    app.engine('html', require('ejs').renderFile);
+
+    app.set('view engine', 'ejs'); // set up ejs for templating
+    app.set('view engine', 'html'); // set up tiny html viewer
+    // required for passport
+    app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+    app.use(passport.initialize());
+    app.use(passport.session()); // persistent login sessions
+    app.use(flash()); // use connect-flash for flash messages stored in session
+
+    // Add static page directory
+    app.use(express.static(__dirname + '/template'));
+
+    // routes ======================================================================
+    // load our routes and pass in our app and fully configured passport
+    require('./app/nmon-routes.js')(app, passport); 
+    // nmon-repo ===================================================================
+    // load nmon-repo module
+    require('./app/nmon-repo.js')(app, passport, log);
+
+    //
+    // launch ======================================================================
+    app.listen(port);
+    console.log('The magic happens on port ' + port);
+}
