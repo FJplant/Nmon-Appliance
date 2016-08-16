e0503827338d8a510f20e24f8e3b4ec65587e49f
Youngmo Kwon <youngmo.kwon777@gmail.com>
2016-08-17 04:36:11 +0900

    change client

--- nmon-repo/config/auth.js
+++ nmon-repo/config/auth.js
@@ -6,8 +6,8 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '296152677408511', // your App ID
         'clientSecret'  : 'bc628cbefdc75c9ff2e5bf5a102d26da', // your App Secret
-        //'callbackURL'   : 'http://nmrep-dev.fjint.com:6900/auth/facebook/callback'
-        'callbackURL'   : 'http://nmdb.nmon.io/auth/facebook/callback'
+        'callbackURL'   : 'http://nmrep-dev.fjint.com:6900/auth/facebook/callback'
+        //'callbackURL'   : 'http://nmdb.nmon.io/auth/facebook/callback'
      },
 
     'twitterAuth' : {
