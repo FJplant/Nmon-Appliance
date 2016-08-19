a1ef28395e529172f97bf5a86cdadb1d06c03a6f
multipia <youngmo.kwon777@gmail.com>
2016-08-20 04:03:29 +0900

    change bower locations

--- nmon-repo/app/nmon-routes.js
+++ nmon-repo/app/nmon-routes.js
@@ -5,6 +5,10 @@ module.exports = function(app, passport) {
 	// HOME PAGE (with login links) ========
 	// =====================================
 	app.get('/', function(req, res) {
+		res.redirect('/nmon-db/v1/');
+	});
+
+	app.get('/nmon-db/v1/', function(req, res) {
 		res.render('index.ejs'); // load the index.ejs file
 	});
 
@@ -12,16 +16,16 @@ module.exports = function(app, passport) {
 	// LOGIN ===============================
 	// =====================================
 	// show the login form
-	app.get('/login', function(req, res) {
+	app.get('/nmon-db/v1/login', function(req, res) {
 
 		// render the page and pass in any flash data if it exists
 		res.render('login.ejs', { message: req.flash('loginMessage') });
 	});
 
 	// process the login form
-	app.post('/login', passport.authenticate('local-login', {
+	app.post('/nmon-db/v1/login', passport.authenticate('local-login', {
 		//successRedirect : '/profile', // redirect to the secure profile section
-		successRedirect : '/v1/nmon-db', // redirect to the secure nmon-db section
+		successRedirect : '/nmon-db/v1/main', // redirect to the secure nmon-db section
 		failureRedirect : '/login', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
@@ -30,16 +34,16 @@ module.exports = function(app, passport) {
 	// SIGNUP ==============================
 	// =====================================
 	// show the signup form
-	app.get('/signup', function(req, res) {
+	app.get('/nmon-db/v1/signup', function(req, res) {
 
 		// render the page and pass in any flash data if it exists
 		res.render('signup.ejs', { message: req.flash('signupMessage') });
 	});
 
 	// process the signup form
-	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/profile', // redirect to the secure profile section
-		failureRedirect : '/signup', // redirect back to the signup page if there is an error
+	app.post('/nmon-db/v1/signup', passport.authenticate('local-signup', {
+		successRedirect : '/nmon-db/v1/profile', // redirect to the secure profile section
+		failureRedirect : '/nmon-db/v1/signup', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
 
@@ -48,7 +52,7 @@ module.exports = function(app, passport) {
 	// =====================================
 	// we will want this protected so you have to be logged in to visit
 	// we will use route middleware to verify this (the isLoggedIn function)
-	app.get('/profile', isLoggedIn, function(req, res) {
+	app.get('/nmon-db/v1/profile', isLoggedIn, function(req, res) {
 		res.render('profile.ejs', {
 			user : req.user // get the user out of session and pass to template
 		});
@@ -63,7 +67,7 @@ module.exports = function(app, passport) {
         // handle the callback after facebook has authenticated the user
         app.get('/auth/facebook/callback',
             passport.authenticate('facebook', {
-                successRedirect : '/profile',
+                successRedirect : '/nmon-db/v1/profile',
                 failureRedirect : '/'
             }));
 
@@ -72,7 +76,7 @@ module.exports = function(app, passport) {
         // =====================================
         // LOGOUT ==============================
         // =====================================
-	app.get('/logout', function(req, res) {
+	app.get('/nmon-db/v1/logout', function(req, res) {
 		req.logout();
 		res.redirect('/');
 	});
