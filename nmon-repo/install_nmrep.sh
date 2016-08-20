1a89f1bdd6efcc675942779808b60af66b216036
multipia <youngmo.kwon777@gmail.com>
2016-08-21 04:58:00 +0900

    change install_npm.sh to install_nmrep.sh

---
+++ nmon-repo/install_nmrep.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir logs
+npm install
+cd ./web-static
+bower install
--- nmon-repo/install_npm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-npm install forever
-npm install swig
-npm install JSONStream
-npm install csv-streamify
-npm install winston
-npm install mongojs
-npm install cluster
-mkdir logs
