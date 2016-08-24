cc3963bedb909ca0fab352886a607eca39275fd4
multipia <youngmo.kwon777@gmail.com>
2016-08-24 13:52:13 +0900

    remove String.format()

--- nmon-repo/app/nmrep-api.js
+++ nmon-repo/app/nmrep-api.js
@@ -359,34 +359,3 @@ function put_nmonlog(req, res, bulk_unit) {
     // in order of request -> parser -> writer
     req.pipe(csvToJson).pipe(parser).pipe(writer);
 }
-
-String.format = function(str, arr) {
-    var i = -1;
-    function callback(exp, p0, p1, p2, p3, p4) {
-        if (exp=='%%') return '%';
-        if (arr[++i]===undefined) return undefined;
-        var exp  = p2 ? parseInt(p2.substr(1)) : undefined;
-        var base = p3 ? parseInt(p3.substr(1)) : undefined;
-        var val;
-        switch (p4) {
-            case 's': val = arr[i]; break;
-            case 'c': val = arr[i][0]; break;
-            case 'f': val = parseFloat(arr[i]).toFixed(exp); break;
-            case 'p': val = parseFloat(arr[i]).toPrecision(exp); break;
-            case 'e': val = parseFloat(arr[i]).toExponential(exp); break;
-            case 'x': val = parseInt(arr[i]).toString(base?base:16); break;
-            case 'd': val = parseFloat(parseInt(arr[i], base?base:10).toPrecision(exp)).toFixed(0); break;
-        }
-        val = typeof(val)=='object' ? JSON.stringify(val) : val.toString(base);
-        var sz = parseInt(p1); /* padding size */
-        var ch = p1 && p1[0]=='0' ? '0' : ' '; /* isnull? */
-        while (val.length<sz) val = p0 !== undefined ? val+ch : ch+val; /* isminus? */
-       return val;
-    }
-    var regex = /%(-)?(0?[0-9]+)?([.][0-9]+)?([#][0-9]+)?([scfpexd])/g;
-    return str.replace(regex, callback);
-}
-
-String.prototype.$ = function() {
-    return String.format(this, Array.prototype.slice.call(arguments));
-}
