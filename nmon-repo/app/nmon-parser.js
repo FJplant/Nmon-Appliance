8f7ec133f0f9e799b7925f72ef7c80e2260685c4
multipia <youngmo.kwon777@gmail.com>
2016-08-29 07:21:12 +0900

    implement NET section

--- nmon-repo/app/nmon-parser.js
+++ nmon-repo/app/nmon-parser.js
@@ -13,7 +13,7 @@ module.exports = NmonParser;
 var fs = require('fs'),
     util = require('util');
 
-// TODO: remove nmdb environment. This is not realted to generic nmon-parser.js
+// TODO: change nmdb environment. This is not realted to generic nmon-parser.js
 var nmdb = require('../config/nmdb-config');
 
 const Transform = require('stream').Transform;
@@ -39,6 +39,7 @@ function NmonParser(options) {
     this._docBBBP = [];
     this._docZZZZ = {};
     this._diskstats = [];
+    this._netstats = [];
     this._rawHeader = {};
     this._cnt = 0;
     this._cntTU = 0;
@@ -83,7 +84,6 @@ NmonParser.prototype._transform = function(chunk, encoding, callback) {
         this.log('\n\033[1;34m[' + now.toLocaleTimeString() + ']-');
         this.log('['+ this._hostname + ':AAA]\033[m ' + chunk[1] + ',' + chunk[2] );
             
-        // TODO: change nmondataid generation policy
         if (chunk[1] === 'time') {
             var time = chunk[2];
             this._docAAA['time'] = time.replace(/\./gi,':');  // fix leading "." before seconds in linux nmon file             
@@ -99,6 +99,7 @@ NmonParser.prototype._transform = function(chunk, encoding, callback) {
 
             //console.log('beginDateTime: ' + beginDateTime + ', datetime: ' + (new Date(beginDateTime)).getTime());
 
+            // TODO: change nmondataid generation policy
             this._nmondataid = this._docAAA['host'] + '$' + 
                                this._docAAA['date'] + '$' + 
                                this._docAAA['time'] + '$' + 
@@ -262,7 +263,6 @@ NmonParser.prototype._transform = function(chunk, encoding, callback) {
         //     'UARG': user process command parameter and informations
         if( chunk[0] in this._rawHeader ) {
             var h = this._rawHeader[chunk[0]];
-            // TODO: Separate val, iops, readkb, writekb, readps, writeps to db
             var val = 0.0, iops = 0.0, 
                 read = 0.0, write = 0.0, 
                 readps = 0.0, writeps = 0.0;
@@ -365,10 +365,18 @@ NmonParser.prototype._transform = function(chunk, encoding, callback) {
                         fields[h[i]] = parseInt(chunk[i]);
                     }
                     else if (h[0] === 'NET') {
-                        if( h[i].indexOf('read') != -1 )
+                        if( h[i].indexOf('read') != -1 ) {
+                            var adapter_idx = i - 2;
+                            this._netstats[adapter_idx]['read-KB/s'] = parseFloat(chunk[i]);
+
                             read += parseFloat(chunk[i]);
-                        else if( h[i].indexOf('write') != -1)
+                        }
+                        else if( h[i].indexOf('write') != -1) {
+                            var adapter_idx = (i - 2) - this._netstats.length;
+                            this._netstats[adapter_idx]['write-KB/s'] = parseFloat(chunk[i]);
+                            
                             write += parseFloat(chunk[i]);
+                        }
                     }
                     else if (h[0] === 'NETPACKET') {
                         if( h[i].indexOf('read/s') != -1 ) {
@@ -378,9 +386,7 @@ NmonParser.prototype._transform = function(chunk, encoding, callback) {
                             writeps += parseFloat(chunk[i]);
                         }
                     }
-                    // TODO: separate DISK statistics.
-                    //       every fields should go to array
-                    //       have to index i - 2 to _diskstats because here is more column before real data
+                    //       have to index (i-2) to _diskstats because here is more column before real data
                     else if (h[0].indexOf("DISKBUSY")== 0 && h[i].match(/.+\d*$/)) {
                         this._diskstats[i-2]['%Busy'] = parseFloat(chunk[i]);
                     }
@@ -400,8 +406,6 @@ NmonParser.prototype._transform = function(chunk, encoding, callback) {
                     else if (h[0].indexOf("DISKBSIZE")== 0 && h[i].match(/.+\d*$/)) {
                         this._diskstats[i-2]['BlockSize'] = parseFloat(chunk[i]);
                     }
-                    // TODO: separate JFS filesystem used %
-                    //       should go to array
                     else if (h[0].indexOf("JFSFILE")== 0) {
                         fields[h[i]] = parseFloat(chunk[i]);
                     }
@@ -436,7 +440,6 @@ NmonParser.prototype._transform = function(chunk, encoding, callback) {
             } // end of for
 
             // write to _docZZZZ
-            // TODO: separate disk read/write, xfer, busy, bsize
             if (Object.keys(fields).length !== 0) {
                 if (h[0].substring(0, 3) === 'CPU' && h[0] !== 'CPU_ALL')
                     this._docZZZZ['CPU'].push(fields);
@@ -460,11 +463,13 @@ NmonParser.prototype._transform = function(chunk, encoding, callback) {
                 this._docZZZZ['DISK_ALL']['iops'] = iops;
             }
             else if (h[0] === 'NET') {
+                this._docZZZZ['NET'] = this._netstats;
+
+                // TODO: remove folllowing lines after chaning logic
                 this._docZZZZ['NET_ALL']['read'] = read;
                 this._docZZZZ['NET_ALL']['write'] = write;
             }
-
-            // set 
+            // end of set summary data
         } // end of if
         else {
             // if found new row header, add it to categories
@@ -472,7 +477,8 @@ NmonParser.prototype._transform = function(chunk, encoding, callback) {
                 this._writer.addCategory({name: chunk[0]});
                 this._rawHeader[chunk[0]] = chunk;
                 
-                // Initialize disk list
+                // Initialize disk list. 
+                // This is necessary because DISKSTSTS spans to multi rows. 
                 if ( chunk[0] === 'DISKBUSY' ) { 
                     for ( var i = 2; i < chunk.length; i++ ) {
                         this._diskstats.push( {
@@ -482,6 +488,17 @@ NmonParser.prototype._transform = function(chunk, encoding, callback) {
                             'WriteKB' : 0.,
                             'tps' : 0.,
                             'BlockSize' : 0.
+                        } );
+                    }
+                }
+
+                if ( chunk[0] === 'NET' ) { 
+                    var netadapter_cnt = (chunk.length - 3)/2;   // due to trailing , chunk.length -3 not -2
+                    for ( var i = 2; i < netadapter_cnt + 2; i++ ) {
+                        this._netstats.push( {
+                            'adapter_id' : chunk[i].split('-')[0],
+                            'read-KB/s' : 0.,
+                            'write-KB/s' : 0.
                         } );
                     }
                 }
