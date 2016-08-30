76e254c965a1f70afdd61a86ae7e98c425bfc238
multipia <youngmo.kwon777@gmail.com>
2016-08-31 02:50:23 +0900

    remove parser log new line on every 80 lines

--- nmon-repo/app/nmon-parser.js
+++ nmon-repo/app/nmon-parser.js
@@ -67,8 +67,7 @@ function NmonParser(options) {
     this._diskstats = [];
     this._netstats = [];
     this._rawHeader = {};
-    this._cnt = 0;
-    this._cntTU = 0;
+    this._cntTU = 0; // counter for parser log formatting
 
     // NmonWriter
     this._writer = new NmonWriter({
@@ -168,10 +167,6 @@ NmonParser.prototype._flush = function(callback) {
 NmonParser.prototype._flushSave = function() {
     if (Object.keys(this._docZZZZ).length !== 0 ) {
         this._writer.writeZZZZ(this._docZZZZ);
-        this._cnt++;
-        //loggerParser.stdout.write('f');
-        if (this._cnt % 80 == 0)
-            this.log('\n');
     } else {
         console.error('Strange _docZZZZ occurred: '  + JSON.stringify(this._docZZZZ));
     }
